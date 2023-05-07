1c26585e70a76cde4505c175f0d0acabe11924de
nathansabourdy <nathan.sabourdy@epitech.eu>
2023-05-07 19:48:20 +0200

    side bar done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import '../css/SideBar.css';
+import { SideBarData } from './SideBarData';
+import { SideBarData2 } from './SideBarData';
+import logo from '../assets/LogoTidaly.png';
 
 function SideBar() {
     return (
         <div className='SideBar'>
+
+            <div className="circle-Container2">
+                <div className="circle2">
+                        <img className="logo" src={logo} alt="Cloudy Sky"></img>
+                        <h2>TIDALY</h2>
+                </div>
+            </div>
+            <ul className='SideBarList'>
+                {SideBarData.map((val, key) => {
+                    return (
+                        <li
+                            key={key}
+                            className='row'
+                            id={window.location.pathname == val.link ? "active" : ""}
+                            onClick={() => {window.location.pathname = val.link}}
+                        >
+                            <div>
+                                <div id ="icon"> {val.icon} </div>
+                                <div id="title"> {val.title} </div>
+                            </div>
+                        </li>
+                    );
+                })}
+                <div className='addSpace'></div>
+                {SideBarData2.map((val, key) => {
+                    return (
+                        <li
+                            key={key}
+                            className='row'
+                            id={window.location.pathname == val.link ? "active" : ""}
+                            onClick={() => {window.location.pathname = val.link}}
+                        >
+                            <div>
+                                <div id ="icon"> {val.icon} </div>
+                                <div id="title"> {val.title} </div>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
---
+++ src/components/SideBarData.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import EdgesensorHighIcon from '@mui/icons-material/EdgesensorHigh';
+import HistoryIcon from '@mui/icons-material/History';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+export const SideBarData = [
+    {
+        title: "Accueil",
+        icon: <HomeIcon />,
+        link: "/HomePage"
+    },
+    {
+        title: "Statistiques",
+        icon: <QueryStatsIcon />,
+        link: "/HomePages"
+    },
+    {
+        title: "Mes capteurs",
+        icon: <EdgesensorHighIcon />,
+        link: "/SensorDetails"
+    },
+    {
+        title: "Historiques de consommation",
+        icon: <HistoryIcon />,
+        link: "/HomePages"
+    },
+    {
+        title: "Profil utilisateur",
+        icon: <PersonIcon />,
+        link: "/HomePages"
+    }
+]
+
+export const SideBarData2 = [
+    {
+        title: "Déconnexion",
+        icon: <LogoutIcon />,
+        link: "/"
+    }
+]
