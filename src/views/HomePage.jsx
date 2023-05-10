a4d2655ef5a04777dc190cd4d4e2aed06066465c
nathansabourdy <nathan.sabourdy@epitech.eu>
2023-05-10 18:47:54 +0200

    finish graphich in homePage

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -14,14 +14,24 @@ import DayOfTheWeek from '../components/DayOfTheWeek.js';
 Chart.defaults.plugins.legend.display = false;
 Chart.defaults.scale.category = Chart.defaults.scale.linear;
 
-const data = {
+let data = {
     labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
     datasets: [
         {
             label: "Consommation en litres d'eau",
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [65, 59, 80, 21, 0, 0, 0],
         }
     ]
+};
+
+let dataCopy = {
+  labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
+  datasets: [
+      {
+          label: "Consommation en litres d'eau",
+          data: [65, 59, 80, 21, 0, 0, 0],
+      }
+  ]
 };
 
 const options = {
@@ -104,10 +114,33 @@ export function getIndexDay(){
 export const HomePage = () => {
     const chartRef = useRef(null);
 
-    const [selectedOption, setSelectedOption] = useState('Année');
+    const [selectedOption, setSelectedOption] = useState('Semaine');
 
     const handleOptionChange = (event) => {
       setSelectedOption(event.target.value);
+
+      if (event.target.value == "Semaine") {
+          data = {
+            labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
+            datasets: [
+                {
+                    label: "Consommation en litres d'eau",
+                    data: [65, 59, 80, 21, 0, 0, 0],
+                }
+            ]
+        };
+      }
+      else {
+        data = {
+          labels: ['Semaine 1', 'Semaine 2', 'Semaine 3' , 'Semaine 4'],
+          datasets: [
+              {
+                  label: "Consommation en litres d'eau",
+                  data: [225, 0, 0, 0],
+              }
+          ]
+      };
+      }
     }
 
     useEffect(() => {
@@ -133,7 +166,7 @@ export const HomePage = () => {
           <div className='rectangle2'>
           <h3 style={{ fontFamily: 'Arial', color: "rgb(102, 102, 102)" , marginLeft:"20px", paddingTop:"15px", textAlign:"left"}}>Consommation du jour</h3>
           <h4 style={{ fontFamily: 'Arial', color: "rgb(102, 102, 102)", marginLeft:"20px", textAlign:"left"}}><DayOfTheWeek/> - {getCurrentDate()}</h4>
-          <CircleChart data={data.datasets.at(0).data.at(getIndexDay())}></CircleChart>
+          <CircleChart data={dataCopy.datasets.at(0).data.at(getIndexDay())}></CircleChart>
           </div>
           <div className='rectangle1'>
             <div className="select-container" style={{ display: "inline-flex", textAlign:"right", position:"absolute", marginLeft:"20%", marginTop:"20px"}}>
