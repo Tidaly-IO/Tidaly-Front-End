4b6247d6de224c57689f189cd43a3a94910b1f40
KillianD1 <72009583+KillianD1@users.noreply.github.com>
2024-09-20 11:02:24 +0200

    feat: update tuto

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -7,15 +7,20 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import home from '../assets/home.png';
+import home from '../assets/homev2.png';
 import accountSetup from "../assets/accountSetup.png";
 import capteur from "../assets/capteurs.png";
 import consoGlobaleCapteurs  from "../assets/conso_globale_des_capteurs.png";
 import profile from "../assets/profile.png";
+import profilev2 from "../assets/profilev2.png";
 import repartitionConsoCapteurs from "../assets/répartition_de_la_conso_des_capteurs.png";
-import stats from "../assets/stats.png";
+import stats from "../assets/statsv2.png";
 import waterMeter from "../assets/waterMeter.png";
 import waterMeterSetup from "../assets/waterMeterSetup.png";
+import monFoyer from "../assets/monFoyer.png";
+import monFoyer2 from "../assets/monFoyer2.png";
+import estimator from "../assets/estimateur.png";
+import feedback from "../assets/vosRetours.png";
 
 const images = [
     {
@@ -31,12 +36,28 @@ const images = [
         imgPath: waterMeterSetup,
     },
     {
+        label: 'Ici, vous pouvez renseigner les informations de votre foyer pour avoir une comparaions de votre consommation d\'eau par rapport à la consommation moyenne des foyers de votre région.',
+        imgPath: monFoyer,
+    },
+    {
+        label: 'Ici, vous pouvez pouvez voir les informations de votre foyer et les modifier si nécessaire. Vous pouvez également ajouter votre foyer si vous avez passé cette étape lors de votre connexion.',
+        imgPath: monFoyer2,
+    },
+    {
         label: 'La page d\'accueil affiche votre consommation d\'eau en temps réel pour la journée en cours, ainsi que la consommation de la semaine actuelle et des semaines précédentes du mois.',
         imgPath: home,
     },
     {
         label: 'La page de profil vous permet de consulter et modifier vos informations personnelles, de changer votre mot de passe, de supprimer votre compte, et d\'accéder au tutoriel si nécessaire.',
-        imgPath: profile,
+        imgPath: profilev2,
+    },
+    {
+        label: 'La page de estimateur vous permez d\'estimez le nombre de capteur par point d\'eau vous avez besoin selon le nombre de point d\'eau dans votre foyer.',
+        imgPath: estimator,
+    },
+    {
+        label: 'La page de vos retours vous permet de nous faire part de vos retours sur l\'application, de nous dire quelle fonctionnalité vous préférée et pourquoi, celle que vous n\'aimez pas et pourquoi, et de nous faire part de vos suggestions pour améliorer l\'application.',
+        imgPath: feedback,
     },
     {
         label: 'Sur la page des statistiques, vous pouvez visualiser votre historique de consommation d\'eau sur la journée, la semaine, le mois et l\'année en cours, ainsi que sur toutes les périodes précédentes disposant d\'un historique. Vous y trouverez également un résumé de votre consommation et de vos économies réalisées.',
