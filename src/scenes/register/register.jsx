8aaa5b5e6a2b8c59981b0c27695a407c0440978a
KillianD1 <72009583+KillianD1@users.noreply.github.com>
2024-11-08 10:04:48 +0100

    feat: add error message for min lenght password and add repartiton de la conso globale par rapport aux capteurs

--- src/scenes/register/register.jsx
+++ src/scenes/register/register.jsx
@@ -4,13 +4,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useTheme } from "@mui/material";
 import { tokens } from "../../theme";
-import logo  from '../../assets/logoTidaly.png';
+import logo from '../../assets/logoTidaly.png';
 
 const Register = () => {
     const [verifyPassword, setVerifyPassword] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showAlertPassword, setShowAlertPassword] = useState(false);
+    const [showAlertPasswordLength, setShowAlertPasswordLength] = useState(false);
     const navigate = useNavigate();
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
@@ -19,9 +20,14 @@ const Register = () => {
     const handleRegister = async (e) => {
         e.preventDefault();
 
+        if (password.length < 8) {
+            setShowAlertPasswordLength(true);
+            return;
+        }
+
         if (password !== verifyPassword) {
             setShowAlertPassword(true);
-            return
+            return;
         }
 
         const userData = {
@@ -35,7 +41,6 @@ const Register = () => {
             console.log('Réponse du serveur:', response.data);
 
             navigate('/');
-
         } catch (error) {
             if (error.response && error.response.status === 422) {
                 setShowAlertEmail(true);
@@ -69,6 +74,11 @@ const Register = () => {
                         <Typography variant="h1" gutterBottom style={{ color: colors.tidaly[100] }}>
                             Inscription
                         </Typography>
+                        {showAlertPasswordLength && (
+                            <Alert severity="error" style={{ width: '100%', marginBottom: '16px' }}
+                                   onClose={() => setShowAlertPasswordLength(false)}>Le mot de passe doit contenir au moins 8 caractères.
+                            </Alert>
+                        )}
                         {showAlertPassword && (
                             <Alert severity="error" style={{ width: '100%', marginBottom: '16px' }}
                                    onClose={() => setShowAlertPassword(false)}>Les mots de passe ne correspondent pas.
--- src/scenes/sharedSensorConsumption/index.jsx
+++ src/scenes/sharedSensorConsumption/index.jsx
@@ -4,17 +4,120 @@ import Header from "../../components/Header";
 import SelectButton from "../../components/SelectButton";
 import { tokens } from "../../theme";
 import { WaterDamage } from "@mui/icons-material";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const SharedSensorConsumption = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const [selectedYear, setSelectedYear] = useState(2024);
+    const [selectedMonth, setSelectedMonth] = useState('');
+    const [totalConsumption, setTotalConsumption] = useState('');
+    const [totalConsumptionSensors, setTotalConsumptionSensors] = useState('');
+    const [globalConsumption, setGlobalConsumption] = useState([]);
+    const [sensorsGlobalConsumption, setSensorsGlobalConsumption] = useState([]);
+    const [activityData, setActivityData] = useState([]);
+
+    const getGlobalConsumption = async () => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+        };
+
+        try {
+            const response = await axios.get('https://tidaly-api-backend.onrender.com/consumption/global?period=year', config);
+            const getGlobalConsumption = response.data.consumption;
+            const getGlobalConsumptionSensors = response.data.sensorsResults;
+
+            setGlobalConsumption(getGlobalConsumption);
+            setSensorsGlobalConsumption(getGlobalConsumptionSensors);
+        } catch (error) {
+            console.error("Erreur lors de la récupération des informations :", error);
+        }
+    };
+
+    const calculateTotalConsumption = () => {
+        const monthMap = {
+            "Janvier": 0, "Février": 1, "Mars": 2, "Avril": 3,
+            "Mai": 4, "Juin": 5, "Juillet": 6, "Août": 7,
+            "Septembre": 8, "Octobre": 9, "Novembre": 10, "Décembre": 11
+        };
+
+        const monthNum = monthMap[selectedMonth];
+        const filteredConsumption = globalConsumption.filter(item => {
+            const date = new Date(item.time);
+            const itemYear = date.getFullYear();
+            const itemMonth = date.getMonth();
+
+            return itemYear === selectedYear && (selectedMonth === '' || itemMonth === monthNum);
+        });
+
+        setTotalConsumption(filteredConsumption.reduce((sum, item) => sum + item.value, 0));
+    };
+
+    const calculateTotalConsumptionOfSensors = () => {
+        const monthMap = {
+            "Janvier": 0, "Février": 1, "Mars": 2, "Avril": 3,
+            "Mai": 4, "Juin": 5, "Juillet": 6, "Août": 7,
+            "Septembre": 8, "Octobre": 9, "Novembre": 10, "Décembre": 11
+        };
+
+        const monthNum = monthMap[selectedMonth];
+        const activityList = [];
+
+        const sensorsConsumption = sensorsGlobalConsumption.map(sensorObj => {
+            const { sensor, data } = sensorObj;
+            const filteredData = data.filter(item => {
+                const date = new Date(item.time);
+                const itemYear = date.getFullYear();
+                const itemMonth = date.getMonth();
+
+                return itemYear === selectedYear && (selectedMonth === '' || itemMonth === monthNum);
+            });
+
+            filteredData.forEach(entry => {
+                activityList.push({
+                    sensorName: sensor.name,
+                    date: new Date(entry.time).toLocaleDateString(),
+                    value: entry.value
+                });
+            });
+
+            return filteredData.reduce((sum, item) => sum + item.value, 0);
+        });
+
+        setActivityData(activityList); // Met à jour l'activité
+        setTotalConsumptionSensors(sensorsConsumption.reduce((sum, consumption) => sum + consumption, 0));
+    };
+
+    useEffect(() => {
+        getGlobalConsumption();
+    }, []);
+
+    useEffect(() => {
+        calculateTotalConsumption();
+        calculateTotalConsumptionOfSensors();
+    }, [selectedYear, selectedMonth, sensorsGlobalConsumption]);
+
+    const handleYearChange = (year) => {
+        setSelectedYear(Number(year));
+        setSelectedMonth('');
+    };
+
+    const handleMonthChange = (month) => {
+        setSelectedMonth(month);
+    };
 
     return (
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="CONSOMMATION GLOBALE DES CAPTEURS" subtitle="Consultez votre consommation d'eau par rapport à votre consommation globale et par capteurs" />
                 <Box>
-                    <SelectButton/>
+                    <SelectButton
+                        onYearChange={handleYearChange}
+                        onMonthChange={handleMonthChange}
+                    />
                 </Box>
             </Box>
 
@@ -31,18 +134,13 @@ const SharedSensorConsumption = () => {
                     p="20px"
                     borderRadius="10px"
                 >
-                    <Box
-                        display="flex"
-                        flexDirection="column"
-                        alignItems="center"
-                        mt="25px"
-                    >
+                    <Box display="flex" flexDirection="column" alignItems="center" mt="25px">
                         <PieChart
                             series={[
                                 {
                                     data: [
-                                        { id: 0, value: 200, label: 'Consommation globale' },
-                                        { id: 1, value: 125, label: 'Consommation des capteurs' },
+                                        { id: 0, value: totalConsumption, label: 'Consommation globale' },
+                                        { id: 1, value: totalConsumptionSensors, label: 'Consommation des capteurs' },
                                     ],
                                 },
                             ]}
@@ -52,6 +150,7 @@ const SharedSensorConsumption = () => {
                     </Box>
                 </Box>
 
+                {/* Section des dernières activités */}
                 <Box
                     gridColumn="span 12"
                     gridRow="span 2"
@@ -59,104 +158,33 @@ const SharedSensorConsumption = () => {
                     p="20px"
                     borderRadius="10px"
                     position="relative"
-                    textAlign="center" // Alignement du texte au centre
                 >
-                    {/* Première paire de "50 L" */}
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '40%', left: '5%' }} />
-                    <Typography
-                        variant="h4"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="40%" // Position du premier texte
-                        left="10%" // Position horizontale de la première paire
-                        transform="translate(-50%, -50%)"
-                        fontWeight="600"
-                    >
-                        Toilettes : 500 L
+                    <Typography variant="h4" fontWeight="600" color={colors.grey[100]} textAlign="left">
+                        Dernières activités
                     </Typography>
 
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '75%', left: '5%' }} />
-                    <Typography
-                        variant="h4"
-                        fontWeight="600"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="75%" // Position du deuxième texte
-                        left="10%" // Position horizontale de la première paire
-                        transform="translate(-50%, -50%)"
-                    >
-                        Douche : 700 L
-                    </Typography>
-
-                    {/* Deuxième paire de "50 L" */}
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '40%', left: '40%' }} />
-                    <Typography
-                        variant="h4"
-                        fontWeight="600"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="40%" // Position du premier texte
-                        left="45%" // Position horizontale de la deuxième paire
-                        transform="translate(-50%, -50%)"
-                    >
-                        Robinet du haut : 150 L
-                    </Typography>
-
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '75%', left: '40%' }} />
-                    <Typography
-                        variant="h4"
-                        fontWeight="600"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="75%" // Position du deuxième texte
-                        left="45%" // Position horizontale de la deuxième paire
-                        transform="translate(-50%, -50%)"
-                    >
-                        Robinet du bas : 50 L
-                    </Typography>
-
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '40%', left: '75%' }} />
-                    <Typography
-                        variant="h4"
-                        fontWeight="600"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="40%" // Position du premier texte
-                        left="80%" // Position horizontale de la deuxième paire
-                        transform="translate(-50%, -50%)"
-                    >
-                        Douche du haut : 150 L
-                    </Typography>
-
-                    <WaterDamage style={{ fontSize: '30px', color: colors.tidaly[100], position: 'absolute', top: '75%', left: '75%' }} />
-                    <Typography
-                        variant="h4"
-                        fontWeight="600"
-                        color={colors.grey[100]}
-                        position="absolute"
-                        top="75%" // Position du deuxième texte
-                        left="80%" // Position horizontale de la deuxième paire
-                        transform="translate(-50%, -50%)"
-                    >
-                        Douche du bas : 50 L
-                    </Typography>
-
-                    <Box
-                        mt="25px"
-                        p="0 30px"
-                        display="flex "
-                        justifyContent="space-between"
-                        alignItems="center"
-                    >
-                        <Box>
-                            <Typography
-                                variant="h4"
-                                fontWeight="600"
-                                color={colors.grey[100]}
+                    <Box mt="20px" display="flex" flexDirection="column" gap="10px" style={{ maxHeight: '250px', overflow: 'auto' }}>
+                        {activityData.map((activity, index) => (
+                            <Box
+                                key={index}
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                p="10px 20px"
+                                borderRadius="10px"
+                                backgroundColor={colors.grey[800]}
                             >
-                                Dernières activités
-                            </Typography>
-                        </Box>
+                                <Box display="flex" alignItems="center">
+                                    <WaterDamage sx={{ color: colors.blueAccent[400], fontSize: "24px", mr: "10px" }} />
+                                    <Typography variant="h6" fontWeight="600" color={colors.grey[100]}>
+                                        {activity.sensorName}
+                                    </Typography>
+                                </Box>
+                                <Typography variant="h6" fontWeight="600" color={colors.grey[100]}>
+                                    {activity.value} L ({activity.date})
+                                </Typography>
+                            </Box>
+                        ))}
                     </Box>
                 </Box>
             </Box>
